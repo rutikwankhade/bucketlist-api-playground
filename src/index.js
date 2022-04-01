e88783ff73a18fdbd68dffca9659e09c4b01df81
rutik wankhade <rutikwankhade2@gmail.com>
2022-04-01 10:40:41 +0530

    add tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Playground, store } from 'graphql-playground-react'
+import './index.css'
+import App from './App'
 
+const rootElement = document.getElementById('root')
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+      <App />
+      
+      <div className="md:w-10/12 mx-auto ">
+      <Playground endpoint='https://bucket-list-api.hasura.app/v1/graphql' />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      </div>
+
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+)
